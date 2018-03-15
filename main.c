fb375425685df96d0f8ebe109465d949744fce21
Albert <albertzjl@163.com>
2018-03-15 11:38:45 +0800

    add base framework of main.c

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+//#include 
+
+#define ARRAY_MAX_LENGTH     128
+#define ARRAY_MAX_VALUE      1000
 
 int main(void) {
-	
+	int *Array;
+	unsigned char ArrayLength;
+	unsigned char i = 0;
+
+	struct timeval tv;
+	unsigned int TimeDiff,TimePoint;
+
+	ArrayLength = rand() % ARRAY_MAX_LENGTH;
+	printf("The array length is %d\n",ArrayLength);
+
+	Array = (int *)malloc(ArrayLength * sizeof(int));
+	if(NULL == Array) {
+		printf("array malloc failed!\n");
+		return 0;
+	}
+
+	for(i = 0;i < ArrayLength;++i) {
+		Array[i] = (rand() % ARRAY_MAX_VALUE) - (ARRAY_MAX_VALUE >> 1);
+		printf("%d,",Array[i]);
+	}
+	printf("\n");
+
+	//1.
+	gettimeofday(&tv,NULL);
+	TimePoint = tv.tv_usec;
+	//code here
+	sleep(1);
+	gettimeofday(&tv,NULL);
+	TimeDiff = tv.tv_usec - TimePoint;
+	printf("The time cost is %d\n",TimeDiff);
+
+	free(Array);
 	printf("Hello world\n");
 	return 0;
 }
