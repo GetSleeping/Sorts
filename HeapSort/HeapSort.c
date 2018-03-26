d2dcad35966b778b90fcc01bfb53ff8a96e33afa
Albert <albertzjl@163.com>
2018-03-26 21:09:20 +0800

    add heap sort code,but not good

--- HeapSort/HeapSort.c
+++ HeapSort/HeapSort.c
@@ -2,8 +2,26 @@
 #include <stdlib.h>
 
 
+
+
+
+
+
+
+
+
+
+
 void HeapSort(int *Array,int ArrayLength)
 {
+	int heapSize = ArrayLength;
+	int i;
 	printf("enter %s\n",__FUNCTION__);
+	
+	buildHeap(Array,ArrayLength);
+	
+	for(i = 0;i < ArrayLength;i++) {
+
+	}
 }
 
